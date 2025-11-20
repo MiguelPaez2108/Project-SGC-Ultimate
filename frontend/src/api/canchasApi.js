c648a0ca83b0542bd65dd0ea10d97929bbc7dcb6
MiguelPaez2108 <josemiguelpaezpena73@gmail.com>
2025-11-19 21:42:34 -0500

    chore: remove file.

--- frontend/src/api/canchasApi.js
+++
@@ -1,6 +0,0 @@
-import axiosClient from "./axiosClient";
-
-export const getCanchas = async () => {
-  const response = await axiosClient.get("/api/canchas");
-  return response.data; // array de canchas
-};
